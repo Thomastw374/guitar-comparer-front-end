279b55d488599a445ce625c77028c13d64828ba8
Thomastw374 <thomas.whiteley46@gmail.com>
2022-09-14 09:35:28 +0100

    Created a form for adding guitars

--- src/components/GuitarSuggestionCard/GuitarSuggestionCard.jsx
+++ src/components/GuitarSuggestionCard/GuitarSuggestionCard.jsx
@@ -1,22 +1,50 @@
 import "./GuitarSuggestionCard.scss"
 
-const GuitarSuggestionCard = ({guitarPicUrl, guitarName, guitarPrice, guitarDescription}) => {
-  guitarDescription = guitarDescription.replace(
-    "Full Description View Full Description ", ""
-  ).substring(0,100) + "...";
+const GuitarSuggestionCard = ({guitarPicUrl, guitarName, guitarPrice, guitarDescription, isForm}) => {
 
-  if(guitarDescription[-1] === " ") {
-    guitarDescription[-1] = ""
-  } 
+  if(!isForm) {
+    guitarDescription =
+      guitarDescription
+        .replace("Full Description View Full Description ", "")
+        .substring(0, 100) + "...";
 
-  return (
-    <div className="guitar-suggestion-card">
-      <img className="guitar-suggestion-card__image" src={guitarPicUrl} alt="" />
-      <h4 className="guitar-suggestion-card__name">{guitarName}</h4>
-      <h5 className="guitar-suggestion-card__price">{guitarPrice}</h5>
-      <p className="guitar-suggestion-card__description">{guitarDescription}</p>
-    </div>
-  );
+    if (guitarDescription[-1] === " ") {
+      guitarDescription[-1] = "";
+    }
+
+    return (
+      <div className="guitar-suggestion-card">
+        <img
+          className="guitar-suggestion-card__image"
+          src={guitarPicUrl}
+          alt=""
+        />
+        <h4 className="guitar-suggestion-card__name">{guitarName}</h4>
+        <h5 className="guitar-suggestion-card__price">{guitarPrice}</h5>
+        <p className="guitar-suggestion-card__description">
+          {guitarDescription}
+        </p>
+      </div>
+    );
+  }
+
+  else{
+    return (
+      <div className="guitar-suggestion-card guitar-suggestion-card--form">
+        <form action="">
+          <label htmlFor="">Guitar picture url (optional):</label>
+          <input type="text" name="" id="" />
+          <label htmlFor="">Guitar name:</label>
+          <input type="text" name="" id="" />
+          <label htmlFor="">Guitar price:</label>
+          <input type="text" name="" id="" />
+          <label htmlFor="">Guitar description:</label>
+          <input type="text" name="" id="" />
+        </form>
+      </div>
+    );
+  }
+  
 };
 
 export default GuitarSuggestionCard;
--- src/components/YourGuitarsContainer/YourGuitarsContainer.jsx
+++ src/components/YourGuitarsContainer/YourGuitarsContainer.jsx
@@ -18,6 +18,7 @@ const YourGuitarsContainer = ({ }) => {
                 "The Ibanez RGMS8 Iron Label Multi Scale 8 String has been sculpted for players traveling to the darkest depths of tonal destruction by offering an extended frequency range and an incredibly comfortable performance. Its multi-scale neck and fingerboard delivers luxury, while the 5-piece maple and walnut construction offers a stable performance. The combination of the mahogany body and jatoba fingerboard provides a balanced tonal character, with plenty of punch and low-end thunder for extra clarity. Its set of Array-8 MS humbuckers are designed to accommodate the extended frequency range, offering powerfully deep tones saturated in sonic thunder. It also features a monorail bridge, which has been built to accommodate its multi-scale design, offering incredible string stability while delivering the perfect articulation across all the strings."
               }
             />
+            <GuitarSuggestionCard isForm="true"/>
           </div>
         </div>
       </>
