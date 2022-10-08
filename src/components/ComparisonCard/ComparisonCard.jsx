a1ef2f5cb6ba9bb5ff2c90d1f747ba9578d56195
Thomastw374 <thomas.whiteley46@gmail.com>
2022-10-08 16:55:06 +0100

    Replaced comparison page html with components

--- src/components/ComparisonCard/ComparisonCard.jsx
+++ src/components/ComparisonCard/ComparisonCard.jsx
@@ -3,28 +3,28 @@ import GenericGuitar from "../../assets/generic-guitar.png"
 
 const ComparisonCard = ({guitar, description}) => {
     return (
-      <div className="comparison-page__guitar-container">
+      <div className="guitar-comparison-card">
         {guitar[0] === "" ? (
           <img
             src={GenericGuitar}
             alt="Generic Guitar"
-            className="comparison-page__guitar-image"
+            className="guitar-comparison-card__guitar-image"
           />
         ) : (
           <img
-            className="comparison-page__guitar-image"
+            className="guitar-comparison-card__guitar-image"
             src={guitar[0]}
             alt="Guitar Pic One"
           />
         )}
-        <h4 className="comparison-page__header">{guitar[1]}</h4>
-        <h4 className="comparison-page__price">{guitar[2]}</h4>
-        <h4 className="comparison-page__header comparison-page__header--specs">
+        <h4 className="guitar-comparison-card__header">{guitar[1]}</h4>
+        <h4 className="guitar-comparison-card__price">{guitar[2]}</h4>
+        <h4 className="guitar-comparison-card__header guitar__header--specs">
           Specifications:
         </h4>
         <div
           dangerouslySetInnerHTML={{ __html: description }}
-          className="comparison-page__description"
+          className="guitar-comparison-card__description"
         ></div>
       </div>
     );
--- src/pages/GuitarComparisonPage/GuitarComparisonPage.jsx
+++ src/pages/GuitarComparisonPage/GuitarComparisonPage.jsx
@@ -3,6 +3,7 @@ import Nav from "../../components/Nav/Nav";
 import GuitarsContext from "../../context/GuitarsContext";
 import "./GuitarComparisonPage.scss";
 import GenericGuitar from "../../assets/generic-guitar.png";
+import ComparisonCard from "../../components/ComparisonCard/ComparisonCard";
 
 const GuitarComparisonPage = () => {
   const { guitarOne, guitarTwo } = useContext(GuitarsContext);
@@ -34,54 +35,8 @@ const GuitarComparisonPage = () => {
     <>
       <Nav />
       <div className="comparison-page">
-        <div className="comparison-page__guitar-container">
-          {guitarOne[0] === "" ? (
-            <img
-              src={GenericGuitar}
-              alt="Generic Guitar"
-              className="comparison-page__guitar-image"
-            />
-          ) : (
-            <img
-              className="comparison-page__guitar-image"
-              src={guitarOne[0]}
-              alt="Guitar Pic One"
-            />
-          )}
-          <h4 className="comparison-page__header">{guitarOne[1]}</h4>
-          <h4 className="comparison-page__price">{guitarOne[2]}</h4>
-          <h4 className="comparison-page__header comparison-page__header--specs">
-            Specifications:
-          </h4>
-          <div
-            dangerouslySetInnerHTML={{ __html: descriptionOne }}
-            className="comparison-page__description"
-          ></div>
-        </div>
-        <div className="comparison-page__guitar-container">
-          {guitarTwo[0] === "" ? (
-            <img
-              src={GenericGuitar}
-              alt="Generic Guitar"
-              className="comparison-page__guitar-image"
-            />
-          ) : (
-            <img
-              className="comparison-page__guitar-image"
-              src={guitarTwo[0]}
-              alt="Guitar Pic Two"
-            />
-          )}
-          <h4 className="comparison-page__header">{guitarTwo[1]}</h4>
-          <h4 className="comparison-page__price">{guitarTwo[2]}</h4>
-          <h4 className="comparison-page__header comparison-page__header--specs">
-            Specifications:
-          </h4>
-          <div
-            dangerouslySetInnerHTML={{ __html: descriptionTwo }}
-            className="comparison-page__description"
-          ></div>
-        </div>
+        <ComparisonCard guitar={guitarOne} description={descriptionOne} />
+        <ComparisonCard guitar={guitarTwo} description={descriptionTwo} />
       </div>
     </>
   );
