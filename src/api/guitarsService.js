bab6e3be38f1d903d0de68be22bc97e4558c4583
Thomastw374 <thomas.whiteley46@gmail.com>
2022-10-15 17:11:29 +0100

    Modified get guitars function to allow pagination

--- src/api/guitarsService.js
+++ src/api/guitarsService.js
@@ -1,9 +1,11 @@
 
 
 // returns data needed by setGuitars 
-export const getGuitars = async (sortBy) => {
+export const getGuitars = async (sortBy, pageNum) => {
   let guitarData = [];
-  const url = `http://localhost:8080/guitars${sortBy ? "?sortBy=" + sortBy : ""}`;
+  const url = `http://localhost:8080/guitars${
+    sortBy ? "?sortBy=" + sortBy : "?sortBy=guitarPrice"
+  }${pageNum ? "&page=" + pageNum : "&page=0"} &page=1`;
   const res = await fetch(url);
   guitarData = await res.json();
   console.log(guitarData);
