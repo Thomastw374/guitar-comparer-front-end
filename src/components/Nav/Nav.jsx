3a800fbe26caf87eb4597e38fb19d57a91369700
Thomastw374 <thomas.whiteley46@gmail.com>
2022-10-20 18:59:55 +0100

    Added logo, edited generic guitar pic, styling adjustments

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,11 +1,13 @@
 import "./Nav.scss"
+import GenericGuitarWhite from "../../assets/generic-guitar-white.png";
 
 const Nav = () => {
-    return(
-        <div className="nav">
-            <h1 className="nav__page-header">GuitarComparer</h1>
-        </div>
-    )
+    return (
+      <div className="nav">
+        <h1 className="nav__page-header">GuitarComparer</h1>
+        <img className="nav__header-guitar" src={GenericGuitarWhite} alt="" />
+      </div>
+    );
 };
 
 export default Nav;
