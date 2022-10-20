bcdadec72f2babf9257f1f96b10b01a56968ffde
Thomastw374 <thomas.whiteley46@gmail.com>
2022-10-20 18:27:16 +0100

    Card styling adjustments

--- src/components/AddGuitarCard/AddGuitarCard.jsx
+++ src/components/AddGuitarCard/AddGuitarCard.jsx
@@ -10,22 +10,22 @@ const AddGuitarCard = ({
 }) => {
   return (
     <div className="guitar-upload">
-      <form action="" onSubmit={addUserGuitar}>
-        <div className="">
-          <label htmlFor="">Guitar picture url (optional):</label>
-          <input type="text" name="" id="" onChange={newGuitarUrl} />
+      <form className="guitar-upload__form" action="" onSubmit={addUserGuitar}>
+        <div className="guitar-upload__form-item">
+          <label className="guitar-upload__form-label" htmlFor="">Guitar Picture URL (optional)</label>
+          <input className="guitar-upload__form-input" type="text" name="" id="" onChange={newGuitarUrl} />
         </div>
-        <div className="">
-          <label htmlFor="">Guitar name:</label>
-          <input type="text" name="" id="" onChange={newGuitarName} />
+        <div className="guitar-upload__form-item">
+          <label className="guitar-upload__form-label" htmlFor="">Guitar Name</label>
+          <input className="guitar-upload__form-input" type="text" name="" id="" onChange={newGuitarName} />
         </div>
-        <div className="">
-          <label htmlFor="">Guitar price:</label>
-          <input type="text" name="" id="" onChange={newGuitarPrice} />
+        <div className="guitar-upload__form-item">
+          <label className="guitar-upload__form-label" htmlFor="">Guitar Price</label>
+          <input className="guitar-upload__form-input" type="text" name="" id="" onChange={newGuitarPrice} />
         </div>
-        <div className="">
-          <label htmlFor="">Guitar description:</label>
-          <input type="text" name="" id="" onChange={newGuitarDescription} />
+        <div className="guitar-upload__form-item">
+          <label className="guitar-upload__form-label" htmlFor="">Guitar Description</label>
+          <textarea cols="30" rows="7" className="guitar-upload__form-input" type="text" name="" id="" onChange={newGuitarDescription} />
         </div>
         <Button text = "Upload Guitar"/>
       </form>
